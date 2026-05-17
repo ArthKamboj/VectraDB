59c6d1b9de20b35658598bcf03bc7c6cafa7f65b
Arth Kamboj <arth23153015@akgec.ac.in>
2026-05-17 12:36:47 +0530

    Updated dashboard urls

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,9 @@
+import os
 import streamlit as st
 import requests
 
 
-API_URL = "http://api-gateway:8000"
+API_URL = os.environ.get("API_URL", "http://localhost:8000")
 
 st.set_page_config(page_title="VectraDB AI", page_icon="🧠", layout="centered")
 
